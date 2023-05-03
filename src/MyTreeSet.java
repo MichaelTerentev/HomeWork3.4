0b23d089f944e9a8eb0163fe8d9b90139a522cde
MichaelTerentev <125468589+MichaelTerentev@users.noreply.github.com>
2023-05-03 18:02:45 +0500

    Update MyTreeSet.java

--- src/MyTreeSet.java
+++ src/MyTreeSet.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class MyTreeSet {
+public class MyTreeSet<E extends Comparable<E>> {
+	private MyTreeMap<E, Object> map;
+	private final Object dummy = new Object();
+	
+	public MyTreeSet() {
+		this.map = new MyTreeMap<>();
+	}
+	public boolean put(E element) {
+		if(!isElementPresent(element)) {
+			map.put(element, dummy);
+		}
+		return !isElementPresent(element);
+	}
+	public void remove(E element) {
+		map.remove(element);
+	}
+	public boolean isElementPresent(E element) {
+		return map.get(element) != null;
+	}
 }
+
