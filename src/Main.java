0713b1a95762e96a558b7d72f11a3d6477218630
MichaelTerentev <125468589+MichaelTerentev@users.noreply.github.com>
2023-05-03 18:02:16 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyTreeSet<Integer> set = new MyTreeSet<>();
+		set.put(1);
+		set.remove(1);
+		System.out.println(set.isElementPresent(1));
     }
 }
